0978bcc0a50c2bc0ec09d734973bd479802a861a
sirajaly <sirajaly50@gmail.com>
2023-12-22 18:13:21 +0500

    Fixed watch video button functionality.

--- js/main.js
+++ js/main.js
@@ -1,38 +1,39 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("side_bar").style.minWidth = "270px";
+}
 
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("side_bar").style.minWidth = "60px";
+}
 
-      function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("side_bar").style.minWidth = "270px";
-      }
+// Add the following code to open the sidebar by default on larger screens
+window.addEventListener("DOMContentLoaded", (event) => {
+  if (window.innerWidth > 768) {
+    openNav();
+  }
+});
 
-      function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("side_bar").style.minWidth = "60px";
-      }
+// Add the following code to close the sidebar when the window is resized to a mobile size
+window.addEventListener("resize", (event) => {
+  if (window.innerWidth <= 768) {
+    closeNav();
+  }
+});
 
-      // Add the following code to open the sidebar by default on larger screens
-      window.addEventListener("DOMContentLoaded", (event) => {
-        if (window.innerWidth > 768) {
-          openNav();
-        }
-      });
+// WATCH VIDEO FUNCTION
+const myDiv = document.getElementById("myDiv");
+const addHeight = document.getElementById("video-2");
 
-      // Add the following code to close the sidebar when the window is resized to a mobile size
-      window.addEventListener("resize", (event) => {
-        if (window.innerWidth <= 768) {
-          closeNav();
-        }
-      });
+const watchVideo = () => {
+  // Check if myDiv has the class 'hidden'
+  if (myDiv.classList.contains("hidden")) {
+    // If true, remove the class 'hidden' from myDiv
+    myDiv.classList.remove("hidden");
+    
+    // Add the class 'add_height' to addHeight (video-2)
+    addHeight.classList.add("add_height");
+  }
+};
 
-
-      const toggleVisibility = () => {
-        const myDiv = document.getElementById('myDiv');
-        const addHeight= document.getElementById('video-2');
-        myDiv.style.display = (myDiv.style.display === 'none' || myDiv.style.display === '') ? 'block' : 'none';
-
-        if (myDiv.style.display === 'block') {
-          addHeight.classList.add('add_height');
-        } else {
-          addHeight.classList.remove('add_height');
-        }
-    }
